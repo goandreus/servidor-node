c81ac6c863492b5e916f7047602232ea3b786320
goandreus <andreshtml5@gmail.com>
2020-01-09 20:34:13 -0500

    graphql

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
 app.get('/',(req, res)=> {
     res.send('Listo')
 });
+
+//resolver
+const root = {hola: () => "Hola Mundo"};
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+
+}));
 
 app.listen(8000, () => console.log('Servidor funcionando'));
